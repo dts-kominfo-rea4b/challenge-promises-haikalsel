2d0e7a37b47b118d039eb94ceaf0fef998c3fe2a
haikalsel <haikalsel@icloud.com>
2022-09-17 11:19:24 +0800

    modifikasi promiseOutput sesuai ketentuan

--- promise.js
+++ promise.js
@@ -1,8 +1,28 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async emosi => {
+    try {
+        const ixx = await promiseTheaterIXX();
+        const vgc = await promiseTheaterVGC();
+
+        // ambil key 'hasil' dari object
+        // Masukkan ke dalam array
+        const hasil = ixx
+            .map(item => item.hasil)
+            .concat(vgc.map(item => item.hasil));
+
+        // filter array yang sesuai dengan
+        // parameter emosi
+        emosi = hasil.filter(hasil => hasil === emosi);
+
+        // output hasil
+        return emosi.length;
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
